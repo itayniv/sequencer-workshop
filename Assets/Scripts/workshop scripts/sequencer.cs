7196f1f79757449f26eb23d3beb6cff2b098ffdc
Itay Niv <itayniv@gmail.com>
2019-01-25 16:05:58 -0500

    update project

--- Assets/Scripts/workshop scripts/sequencer.cs
+++ Assets/Scripts/workshop scripts/sequencer.cs
@@ -1,62 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
 
 
 
-public class sequencer : NetworkBehaviour
-{
-
+public class sequencer : MonoBehaviour{
+    
+    // Declaration of a pad
     public GameObject pad;
+
+    //declaration of an array of materials
     public Material[] materials;
+
+    //declaration of a counter
     private int counter = 0;
+
+    //decleration of all the sounds --> one for each instrument.
     public AudioClip _hiHat, _baseDrum, _snareDrum, _sound01, _sound02, _sound03, _sound04, _sound05;
+
+    //
     private int padNumbers = -1;
+
+    //declaration of an array of booleans that would hold the state of the sequencer (all nontation)
     public bool[] patterns;
 
-
-   
+    //declaration of the an array that would hold the current state of the sequencer in (what step is currently playing)
     public int[] pattern;
 
 
+    //declaration of a list of game objects to hold all pads
     public List<GameObject> _PadsArr = new List<GameObject>();
 
+
+    //declaration of instrument number
     public int instruments = 3;
+
+    //declaration of number of steps
     private int steps = 8;
 
+
+    //declarationof soundmanager script
     public SoundManager _soundManager;
 
 
 
-    AudioSource audioSource;
+    private AudioSource audioSource;
 
     // Use this for initialization
     void Start()
     {
-
+        //
         audioSource = GetComponent<AudioSource>();
 
-        //declare length of array
+        //declare length of array of patterns --> this would hold the map to which pads to play
         patterns = new bool[instruments * steps];
+
+        //Declare length of pattern array --> this would hold the number of steps ( [0] [0] [0] [0] [0] [0] [0] [0] )
         pattern = new int[steps];
 
 
+
+        //reset pattern array to 0
         for (int x = 0; x < steps; x++)
         {
             pattern[x] = 0;
         }
 
 
+
+        //create all pads on the scene 2D array
         for (int y = 0; y < instruments; y++)
         {
             for (int x = 0; x < steps; x++)
             {
-                //Debug.Log(instruments);
+                //This is for the name of pads -->each should have a uniqy name.
                 padNumbers++;
+
+                //instantiat gameobjects Pad
                 GameObject _pad = Instantiate(pad);
+
+                //give pad a uniqu name
                 _pad.name = "pad" + padNumbers;
+
+                //place pad in a uniqu position
                 _pad.transform.position = new Vector3(x, 0, y);
+
+                //add a material for pad from the array of materials
                 _pad.GetComponent<Renderer>().material = materials[0];
                 _PadsArr.Add(_pad);
 
@@ -73,7 +102,7 @@ public class sequencer : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        // clear pattern for curser
+        // for all the pads that are currently on, assign a specific material
         for (int z = 0; z < instruments * steps; z++)
         {
             if (patterns[z] == true)
@@ -83,15 +112,19 @@ public class sequencer : NetworkBehaviour
         }
 
 
+
+        //on each beat from the metronome do the following:
         if (BperM._beatD8)
         {
-            
+               
             if (BperM._beatD8 && BperM._beatCountD8 % 2 == 0)
             {
-                
+                // add step
                 counter++;
 
 
+
+                //if step is larger than 8 than reset
                 if (counter >= 8)
                 {
                     counter = 0;
@@ -105,10 +138,13 @@ public class sequencer : NetworkBehaviour
                 }
 
 
+
+
+                // assign material for cursor, etc.
                 for (int i = 0; i < instruments * steps; i++)
                 {
 
-
+                    //for each step number do the following (switch statement):
                     switch (counter)
                     {
                         //for voice #1
@@ -116,7 +152,9 @@ public class sequencer : NetworkBehaviour
 
                             if (patterns[i] != true)
                             {
+                                //clear the color
                                 _PadsArr[i].GetComponent<Renderer>().material = materials[0];
+                                //than assign it a "playing color" color
                                 _PadsArr[0].GetComponent<Renderer>().material = materials[2];
                                 _PadsArr[0 + steps * 1].GetComponent<Renderer>().material = materials[2];
                                 _PadsArr[0 + steps * 2].GetComponent<Renderer>().material = materials[2];
@@ -148,8 +186,6 @@ public class sequencer : NetworkBehaviour
                             break;
 
 
-                  
-
                         case 2:
                             if (patterns[i] != true)
                             {
@@ -167,7 +203,7 @@ public class sequencer : NetworkBehaviour
 
                             break;
 
-                 
+
 
                         case 3:
                             if (patterns[i] != true)
@@ -222,7 +258,7 @@ public class sequencer : NetworkBehaviour
 
                             break;
 
-                  
+
 
                         case 6:
                             if (patterns[i] != true)
@@ -241,7 +277,7 @@ public class sequencer : NetworkBehaviour
 
                             break;
 
-                 
+
 
                         case 7:
                             if (patterns[i] != true)
@@ -259,54 +295,60 @@ public class sequencer : NetworkBehaviour
                             }
 
                             break;
-
                     }
-
-
                 }
 
 
-                //move Click
+                //move counter on each counter --> [1],[0],[0],[0],[0],[0],[0],[0] --> [0],[1],[0],[0],[0],[0],[0],[0]
                 pattern[counter] = 1;
 
+                //for all the steps check what to play
                 for (int i = 0; i < steps; i++)
                 {
 
+                    //for first row of pads if a pad is pressed and the cursor is at the position than play
                     if ((patterns[(8 * 0) + i] == true) && pattern[i] == 1)
                     {
                         _soundManager.playSound(_baseDrum, 0.1f);
                     }
 
+                    //for first row of pads if a pad is pressed and the cursor is at the position than play
                     if ((patterns[(8*1)+i] == true) && pattern[i] == 1)
                     {
                         _soundManager.playSound(_hiHat, 0.1f);
                     }
 
+                    //for first row of pads if a pad is pressed and the cursor is at the position than play
                     if ((patterns[(8 * 2) + i] == true) && pattern[i] == 1)
                     {
                         _soundManager.playSound(_snareDrum, 0.1f);
                     }
 
+                    //for first row of pads if a pad is pressed and the cursor is at the position than play
                     if ((patterns[(8 * 3) + i] == true) && pattern[i] == 1)
                     {
                         _soundManager.playSound(_sound01, 0.1f);
                     }
 
+                    //for first row of pads if a pad is pressed and the cursor is at the position than play
                     if ((patterns[(8 * 4) + i] == true) && pattern[i] == 1)
                     {
                         _soundManager.playSound(_sound02, 0.1f);
                     }
 
+                    //for first row of pads if a pad is pressed and the cursor is at the position than play
                     if ((patterns[(8 * 5) + i] == true) && pattern[i] == 1)
                     {
                         _soundManager.playSound(_sound03, 0.1f);
                     }
 
+                    //for first row of pads if a pad is pressed and the cursor is at the position than play
                     if ((patterns[(8 * 6) + i] == true) && pattern[i] == 1)
                     {
                         _soundManager.playSound(_sound04, 0.1f);
                     }
 
+                    //for first row of pads if a pad is pressed and the cursor is at the position than play
                     if ((patterns[(8 * 7) + i] == true) && pattern[i] == 1)
                     {
                         _soundManager.playSound(_sound05, 0.1f);
@@ -323,38 +365,37 @@ public class sequencer : NetworkBehaviour
 
 
 
+
+
+
+    // from raycasting script
     public void patternCheck(GameObject padClicked)
     {
-        //Debug.Log(padClicked);
-
-        Debug.Log(padClicked.transform.localScale.y);
-
-
+        
         string thisPadName = padClicked.name;
-        //Debug.Log(thisPadName);
 
+
+        //for the length of the padds Array
         for (var i = 0; i < _PadsArr.Count; i++)
         {
-           
-            //Determine which pad plays and which does not.
-
+            //Determine which pad plays and which should not by name
             string checkName = _PadsArr[i].name;
+
             if (thisPadName == checkName)
             {
-                //Debug.Log(checkName + " " + i);
 
+                //for the pressed gameobjects check if the number of array is true or false
                 if (patterns[i] == true)
                 {
+                    //if it is true(pressed) make it unpressed
                     patterns[i] = false;
                     _PadsArr[i].transform.localScale = new Vector3 (0.8f, .4f, 0.8f);
-                    //Debug.Log(patterns[i]);
                 }
                 else
                 {
-                    Debug.Log(_PadsArr[i].transform.localScale.y);
+                    //if it is false(unpressed) make it pressed
                     _PadsArr[i].transform.localScale = new Vector3 (0.8f, 0.2f, 0.8f);
                     patterns[i] = true;
-                    //Debug.Log(patterns[i]);
                 }
 
             }
